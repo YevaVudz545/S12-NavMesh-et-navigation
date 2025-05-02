fa4fc6b30428a519803b7ce462110632caf3733e
YevaVudz545 <yevavudz545@gmail.com>
2025-05-02 14:52:31 -0400

    agent va a la position initiale

---
+++ Assets/AgentBase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentBase : MonoBehaviour
+{
+    [SerializeField] GameObject _joueur;
+
+    NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_joueur.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
